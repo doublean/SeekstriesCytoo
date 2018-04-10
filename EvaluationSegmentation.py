8f74019f6ea164f0a0bd095ea90e4f6c9a3ecf27
Leduc <polchop@live.fr>
2018-04-10 14:12:37 +0200

    Premier test

---
+++ EvaluationSegmentation.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def evaluation(imgRef,imgTest):
+    """
+    Evaluation de la qualité d'une segmentation 
+    :param imgRef: 
+    :param imgTest: 
+    :return: recouv : pourcentage de recouvrement
+             erreur : pourcentage d'erreur
+    """
+
+
+
+
+
+
+
+
+#main  test
